b5df6cf052ea9b800ce67dcdb8c711e40ae91f5e
Urko <ikumendibe25@lhusurbil.eus>
2025-11-18 08:29:00 +0100

    Trello esteka

---
+++ src/EstekakEtaScrumAzalpena.java
@@ -0,0 +1,18 @@
+public class EstekakEtaScrumAzalpena {
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void main(String[] args) {
+        estekakInprimatu();
+    }
+
+    public static void estekakInprimatu() {
+        System.out.println(ANSI_GREEN + "\nTRELLO ESTEKA: " + ANSI_WHITE + "https://trello.com/b/C6ZU5y3t/zinemausurbil");
+    }
+}
