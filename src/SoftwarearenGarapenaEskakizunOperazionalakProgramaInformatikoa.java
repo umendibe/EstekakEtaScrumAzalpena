de9686563eb6346386ef0dd6a412b644908b0363
Urko <ikumendibe25@lhusurbil.eus>
2025-11-18 09:10:45 +0100

    Eskakizun funtzionalak azalpena

--- src/SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa.java
+++ src/SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa.java
@@ -1,3 +1,23 @@
 public class SoftwarearenGarapenaEskakizunOperazionalakProgramaInformatikoa {
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void main(String[] args) {
+        eskakizunFuntzionalak();
+        
+    }
+
+    public static void eskakizunFuntzionalak() {
+        System.out.println(ANSI_RED + "\nESKAKIZUN FUNTZIONALAK: \n" + ANSI_WHITE +
+                "Eskakizun funztionalak bezero batek produktu bat eskatzen duenean hark bere bezeroeri eskeini diezaiokeen funtzionalitateak dira. Adibide batekin azaltzea errazagoa da. "
+                + "Demagun web orri bat eskatu duela, kasu honetan bere zine enpresarako. Esan digu webak izan behar duela hainbat atal (kartelera, saskia...) eta saskia atalan adibidez sarrerak erosteko funtzionalitatea sartzea eskatu digu. Interaktuatzeko funtzio bat denez, eskakizun funtzionala dela esan daiteke.");
+    }
+
     
 }
